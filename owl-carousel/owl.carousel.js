160817658873ea6c6ef28927b0fd8b7fad0ba5e9
Gerald Terrell Jr <geraldterrelljr@gmail.com>
2015-07-12 02:53:07 +0000

    fixed Owl Carousel on homepage

--- owl-carousel/owl.carousel.js
+++ owl-carousel/owl.carousel.js
@@ -1509,4 +1509,19 @@ if (typeof Object.create !== "function") {
         startDragging : false,
         afterLazyLoad: false
     };
+          $(document).ready(function() {
+        $("#owl-demo").owlCarousel({
+          navigation : true, // Show next and prev buttons
+          slideSpeed : 300,
+          paginationSpeed : 400,
+          singleItem:true
+     
+          // "singleItem:true" is a shortcut for:
+          // items : 1, 
+          // itemsDesktop : false,
+          // itemsDesktopSmall : false,
+          // itemsTablet: false,
+          // itemsMobile : false
+        });
+      });  
 }(jQuery, window, document));
